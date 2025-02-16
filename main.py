3dcf0fa11dc1a037a26eced5be6561e3a0a8dd2c
S-dweep <mondalsayandeep2004@gmail.com>
2025-02-16 23:44:07 +0530

    adding project titles in columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -9,7 +10,7 @@ with col1:
 
 with col2:
     st.title("Sayandeep Mondal")
-    content = """Hi, I am Sayandeep. I am an aspiring Data Scientist and Machine Learning enthusiast, 
+    content="""Hi, I am Sayandeep. I am an aspiring Data Scientist and Machine Learning enthusiast, 
     currently pursuing B.Tech in Computer Science and Technology from Narula Institute of Technology.  
     My passion lies in transforming complex data into meaningful insights and leveraging machine 
     learning algorithms to solve real-world problems. My goal is to further develop my skills in 
@@ -18,6 +19,17 @@ with col2:
     passion for data-driven solutions."""
     st.info(content)
 
-content2 = "Below you can find some of the apps I have built in Python. Do check them out and feel free to contact me!!"
+content2="Below you can find some of the apps I have built in Python. Do check them out and feel free to contact me!!"
 st.write(content2)
 
+col3, col4=st.columns(2)
+
+df=pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[0:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
