c68c98a3ba90064312ac629dbfbf2965e0cb38c3
S-dweep <mondalsayandeep2004@gmail.com>
2025-02-17 02:37:38 +0530

    last touch..

--- send_email.py
+++ send_email.py
@@ -1,11 +1,13 @@
+import os
 import smtplib, ssl
+import os
 
 
 def sendEmail(message):
     host = "smtp.gmail.com"
     port = 465
     username = "mondalsayandeep2004@gmail.com"
-    password = "dpcfxfsovtctywkf"
+    password = os.getenv("PASSWORD_PF")
     receiver = "mondalsayandeep2004@gmail.com"
     context_msg = ssl.create_default_context()
 
